296e38caa6481e015ce1bd1b3c9e6579a5d9f481
oleyon <160229154+Santiago-nve@users.noreply.github.com>
2024-03-07 12:54:09 +0300

    add panel and links to App.vue

--- vue/src/components/ui/NavigationPanel.vue
+++ vue/src/components/ui/NavigationPanel.vue
@@ -1,40 +1,51 @@
 <template>
-    <div class="navigation-panel">
+  <div class="navigation-panel">
+    <RouterLink to="/settings">
       <SimpleButton type="info">Настройки</SimpleButton>
+    </RouterLink>
+    <RouterLink to="/">
       <SimpleButton type="warning">В центр</SimpleButton>
+    </RouterLink>
+    <RouterLink to="/">
       <SimpleButton type="danger">Удалить</SimpleButton>
+    </RouterLink>
+    <RouterLink to="/create">
       <SimpleButton type="primary">Добавить</SimpleButton>
-    </div>
-  </template>
-  
-  <script>
-  import SimpleButton from "./SimpleButton.vue";
-  
-  export default {
-    components: {
-      SimpleButton,
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .navigation-panel {
-    display: flex;
-    justify-content: space-between;
-    font-family: "Montserrat", sans-serif;
-    padding: 10px;
-  }
-  
-  .navigation-panel .simple-button {
-    flex: 1;
-    width: 150px;
-    margin-right: 10px;
-    color: #000000c7;
-    font-weight: 800;
-  }
-  
-  .navigation-panel .simple-button:last-child {
-    margin-right: 0;
-  }
-  </style>
-  
+    </RouterLink>
+  </div>
+</template>
+
+<script>
+import SimpleButton from "./SimpleButton.vue";
+
+export default {
+  components: {
+    SimpleButton,
+  },
+};
+</script>
+
+<style scoped>
+.navigation-panel {
+  display: flex;
+  justify-content: space-between;
+  font-family: "Montserrat", sans-serif;
+  padding: 10px;
+}
+
+.navigation-panel .simple-button {
+  flex: 1;
+  color: #000000c7;
+  font-weight: 800;
+}
+
+.navigation-panel a {
+  flex: 1;
+  display: flex;
+  margin-right: 10px;
+}
+
+.navigation-panel a:last-child {
+  margin-right: 0;
+}
+</style>
